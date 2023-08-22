846593707293300e3138f7b06c87b80032e4f8a2
Lucas Correa <correa97lukas@hotmail.com>
2023-08-22 10:35:41 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,88 +1,39 @@
 import { WebView } from 'react-native-webview';
-import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
-import * as Device from 'expo-device';
+import { useEffect } from 'react'
+import { Platform, SafeAreaView, StyleSheet, Linking } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import Constants from "expo-constants";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
   }),
 });
 
 export default function App() {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
 
   useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title="Press to schedule a notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
-    </View>
-  );
-}
-
-async function schedulePushNotification() {
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: "You've got mail! 📬",
-      body: 'Here is the notification body',
-      data: { data: 'goes here' },
-    },
-    trigger: { seconds: 2 },
+    registerForPushNotificationsAsync().then(token => sendTokenToApi(token));
   });
-}
 
-async function registerForPushNotificationsAsync() {
-  let token;
+  setInterval(() => {
+    this.webref.injectJavaScript(script());
+  }, 100);
 
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
+  const registerForPushNotificationsAsync = async () => {
+    let token;
 
-  if (Device.isDevice) {
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
     if (existingStatus !== 'granted') {
@@ -96,9 +47,70 @@ async function registerForPushNotificationsAsync() {
     token = (await Notifications.getExpoPushTokenAsync({
       projectId: Constants.expoConfig.extra.eas.projectId,
     })).data;
-  } else {
-    alert('Must use physical device for Push Notifications');
+
+    return token;
   }
 
-  return token;
+  const script = () => {
+    return `document.getElementById('resumenDisponible').onclick = function() {
+      window.ReactNativeWebView.postMessage('downloadResumen')
+      };
+      document.getElementById('detalleDisponible').onclick = function() {
+        window.ReactNativeWebView.postMessage('downloadDetalle')
+        };        
+    true;`
+  }
+
+  const sendTokenToApi = (token) => {
+    fetch('https://esmar.app/api/tokens', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: token,
+      }),
+    });
+  }
+
+  const onMessage = async (event) => {
+    const {
+      nativeEvent: { data },
+    } = event;
+    if (data === 'downloadResumen') {
+      await Linking.openURL("https://esmar.app/api/propietarios/resumenDisponible?idUsuarioRemoto=0001")
+    }
+    if (data === 'downloadDetalle') {
+      await Linking.openURL("https://esmar.app/api/propietarios/detalleDisponible?idUsuarioRemoto=1")
+    }
+  };
+
+  const runBeforeFirst = `
+  window.isNativeApp = true;
+  true; // note: this is required, or you'll sometimes get silent failures
+`;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView source={{ uri: 'https://esmar.app' }}
+        ref={(r) => (this.webref = r)}
+        injectedJavaScriptBeforeContentLoaded={runBeforeFirst}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        allowFileAccess={true}
+        allowUniversalAccessFromFileURLs={true}
+        allowFileAccessFromFileURLs={true}
+        allowingReadAccessToURL={true}
+        mixedContentMode={'always'}
+        onMessage={onMessage}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
